0ba893d5a51a6d7c8e258dcb474bfc5af861c331
Brandon Wade <bwade.95@hotmail.com>
2020-12-01 21:44:37 +0000

    added player factory function

--- js/app.js
+++ js/app.js
@@ -6,8 +6,17 @@ const DOM = (() => {
 
 // Create a gameboard for the tic-tac-toe game
 var GameBoard = ((container) => {
-
+    let gameBoard = new Array(9);
 })(document.querySelector('game-container'));
+
+// Create player factory function
+const Player = (marker) => {
+    this.marker = marker;
+    const getMarker = () => this.marker;
+    const setMarker = (sign, active) => {
+
+    };
+}
 
 // Generate Game Loop
 const Game = (player, selection) => {
